5db292d7d517735f1eb48381d59889c2b6a826bb
jackie <jackie9123@pic.net.tw>
2018-03-15 16:54:23 +0800

    新增專案

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caption = string.Empty;
+            string manufacturer = string.Empty;
+            string model = string.Empty;
+            string datawidth = string.Empty;
+            ManagementObjectSearcher mos = new ManagementObjectSearcher();
+
+
+            mos.Query.QueryString = "SELECT * FROM Win32_OperatingSystem";
+            var osInfo = mos.Get().Cast<ManagementObject>().FirstOrDefault();
+            caption = osInfo?.Properties["Caption"]?.Value.ToString();
+            datawidth = osInfo?.Properties["OSArchitecture"]?.Value.ToString();
+
+            mos.Query.QueryString = "SELECT * FROM Win32_ComputerSystem";
+            var mainboardInfo = mos.Get().Cast<ManagementObject>().FirstOrDefault();
+            manufacturer = mainboardInfo?.Properties["Manufacturer"]?.Value.ToString();
+            model = mainboardInfo?.Properties["Model"]?.Value.ToString();
+
+            Console.WriteLine($"{caption}|{manufacturer}|{model}|{datawidth}");
+            Console.ReadLine();
+        }
+    }
+}
